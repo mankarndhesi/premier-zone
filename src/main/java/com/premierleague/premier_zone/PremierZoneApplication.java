e536eebe5c7e5dd4d6123ab8d121513f913f28c9
mankarndhesi <mankarndhesi@gmail.com>
2025-05-30 11:42:07 -0700

    Initial commit

---
+++ src/main/java/com/premierleague/premier_zone/PremierZoneApplication.java
@@ -0,0 +1,14 @@
+package com.premierleague.premier_zone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PremierZoneApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Premier Zone Application");
+		SpringApplication.run(PremierZoneApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/premierleague/premier_zone/player/Player.java
@@ -0,0 +1,9 @@
+package com.premierleague.premier_zone.player;
+
+/*
+    This is our Entity class -> entities represent data models in your application
+    They map directly to database tables
+ */
+
+public class Player {
+}
